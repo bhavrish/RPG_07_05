b848a6249f2b156b838be41c0c52b039362cdbbc
En Yu Zhang <Flashlight@Ens-MacBook-Pro.local>
2017-12-05 18:14:33 -0500

    uploaded version of character file

--- v2/Character.java
+++ v2/Character.java
@@ -1,1 +1,22 @@
+public class Character{
+    protected int life; 
+    protected int strength;
+    protected int defense;
+    protected double attackRating;
 
+    public boolean isAlive(){
+        return life > 0;
+    }
+        
+    public int getDefense(){
+        return defense;
+    }
+
+    public void lowerHP(int loss){
+        life -= loss; 
+    }
+
+    public int attack(Character role){
+        int damage = (strength * attackRating) - role.defense;
+        return damage;
+}

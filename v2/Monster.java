ebf20423f7b96783959fc77c121abb27539e4eda
En Yu Zhang <Flashlight@Ens-MacBook-Pro.local>
2017-12-06 04:03:17 -0500

    added code

---
+++ v2/Monster.java
@@ -0,0 +1,9 @@
+public class Monster extends Character{
+    
+        public Monster(){
+            life = 150;
+            strength = (int)Math.random() * 45 + 20;
+            defense = 20;
+            attack = 1; 
+        }
+    }
